33a66ce43338b533b6fa54b1bccf88cf085541f9
Tajuddin <mohdtajuddin96@hotmail.com>
2023-12-12 11:11:47 +0530

    second commit

--- dbConfig.js
+++ dbConfig.js
@@ -1,10 +1,14 @@
-const Pool=require('pg').Pool
+const { Pool } = require('pg')
 
-const db=new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'postgres',
-    password:'password@123',
-    port:5432
-})
-module.exports=db
+const poolConfig = { max: 5, min: 2, idleTimeoutMillis: 600000 }
+const DataBase = 'postgress_6w6d'
+const UserName = 'postgress'
+const Password = 'drJbFClhWqnxLUYvs214OVdYHGbBnsyc'
+const Host = 'dpg-clrfdcie9h4c73avg0n0-a.oregon-postgres.render.com'
+const Port = 5432
+
+poolConfig.connectionString = `postgres://${UserName}:${Password}@${Host}:${Port}/${DataBase}`
+
+const db = new Pool(poolConfig)
+
+module.exports = db
