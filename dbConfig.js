952faf9ae8efa78800294b4d774f14271ecf7fea
Tajuddin <mohdtajuddin96@hotmail.com>
2023-12-12 17:21:16 +0530

    fourth commit5

--- dbConfig.js
+++ dbConfig.js
@@ -5,7 +5,7 @@ const poolConfig = { max: 5, min: 2, idleTimeoutMillis: 600000 }
 const DataBase = 'ems_pg_db'
 const UserName = 'ems_pg_db_user'
 const Password = 'GOucjjRTqNJrIGtp2SEIR0viSx5DafyM'
-const Host = 'dpg-clrvo3bip8as73a195o0-a.singapore-postgres.render.com'
+const Host = 'dpg-clrvo3bip8as73a195o0-a'
 const Port = 5432
 
 poolConfig.connectionString = `postgres://${UserName}:${Password}@${Host}:${Port}/${DataBase}`
