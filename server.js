8d5dd53762283c456f819cdcfebe9e560c01d256
Tajuddin <mohdtajuddin96@hotmail.com>
2023-12-13 20:20:13 +0530

    fourth commit17

--- server.js
+++ server.js
@@ -3,22 +3,17 @@ const createError = require('http-errors')
 const studentRoutes = require('./src/student/routes')
 const empRoutes = require('./src/employee/routes')
 const errHandler = require('./helper/errorHandler')
-const session = require('express-session')
+
 const app = express()
 
 const Port = 3000
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(session({ secret: 'secret key', saveUninitialized: true, resave: false }))
+
 app.use('/', empRoutes)
 app.use('/api/v1/students', studentRoutes)
 
-app.use((req, res, next) => {
-    res.locals.message = req.session.message;
-    delete req.session.message;
-    next()
-})
 app.use(express.static("uploads"))
 app.set('view engine', 'ejs')
 app.use(async (req, res, next) => { next(createError.NotFound()) })
